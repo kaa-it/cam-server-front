ea5501cd35a86d1719e8e381b93b829887a4b435
Andrey Kruglov <kruglov.it@gmail.com>
2018-11-22 09:06:17 +0300

    Analytics pages are fixed

--- src/components/Settings/Analytics/MotionDetection.vue
+++ src/components/Settings/Analytics/MotionDetection.vue
@@ -1,54 +1,54 @@
 <template>
-  <v-container fluid grid-list-xl>
+  <v-container fluid>
     <v-layout row wrap>
       <v-flex d-flex xs12>
-        <h1>{{ $t("motion_detection.title") }}</h1>
+        <span class="headline font-weight-bold">{{ $t("motion_detection.title") }}</span>
       </v-flex>
       <v-flex xs12 sm7 order-xs2 order-sm1>
-        <v-card>
-          <v-card-title class="yellow darken-1 mb-2">
-            <h3 class="headline">{{ $t("motion_detection.motion_detection_card_title") }}</h3>
+        <v-card class="my-3">
+          <v-card-title class="yellow darken-1 mb-3">
+            <span class="title">{{ $t("motion_detection.motion_detection_card_title") }}</span>
           </v-card-title>
           <v-layout row wrap align-center>
             <v-flex d-flex xs12>
               <v-select class="px-3" :label="$t('motion_detection.motion_detection_mode')" :items="motion_detection_mode" v-model="current_motion_detection_mode"/>
-            </v-flex>  
-            <v-radio-group class="px-4" v-model="radioGroup">
+            </v-flex>
+            <v-radio-group class="px-3" v-model="radioGroup">
               <v-layout row wrap align-center>
               <v-flex xs6>
                 <v-radio :disabled="!motion_detection_enabled" value="1" :label="$t('motion_detection.sensitivity_level')"/>
               </v-flex>
               <v-flex xs5 offset-xs1>
-                <v-select :disabled="!motion_detection_enabled" :items="sensitivity_levels" v-model="current_sensitivity_level"/> 
+                <v-select :disabled="!motion_detection_enabled" :items="sensitivity_levels" v-model="current_sensitivity_level"/>
               </v-flex>
               <v-flex xs6>
                 <v-radio :disabled="!motion_detection_enabled" value="2" :label="$t('motion_detection.customized_sensitivity_level')"/>
               </v-flex>
               <v-flex xs5 offset-xs1>
-                <v-slider :disabled="!motion_detection_enabled" v-model="customized_sensitivity_level" thumb-label="always" :thumb-size="24"/> 
+                <v-slider :disabled="!motion_detection_enabled" v-model="customized_sensitivity_level" thumb-label="always" :thumb-size="24"/>
               </v-flex>
-              </v-layout>    
+              </v-layout>
             </v-radio-group>
             <v-flex xs12>
-              <v-subheader class="px-3">{{ $t("motion_detection.region_of_interest") }}</v-subheader> 
+              <v-subheader class="px-3">{{ $t("motion_detection.region_of_interest") }}</v-subheader>
             </v-flex>
             <v-flex xs12>
               <v-btn :disabled="!motion_detection_enabled" class="px-3" round color="purple">{{ $t("motion_detection.select_all") }}</v-btn>
               <v-btn :disabled="!motion_detection_enabled" class="px-3" round color="purple">{{ $t("motion_detection.clear_all") }}</v-btn>
-            </v-flex>        
+            </v-flex>
           </v-layout>
-        </v-card>    
+        </v-card>
       </v-flex>
       <v-flex xs12 sm5 order-xs1 order-sm2>
-        <v-card>
-          <v-card-title class="yellow darken-1 mb-2">
-            <h3 class="headline">{{ $t("player.title") }}</h3>
+        <v-card :class="{'mt-3': true, 'ml-0': $vuetify.breakpoint.xsOnly, 'ml-3': $vuetify.breakpoint.smAndUp}">
+          <v-card-title class="yellow darken-1 mb-3">
+            <span class="title">{{ $t("player.title") }}</span>
           </v-card-title>
           <v-card-text>
             <VideoPlayer/>
           </v-card-text>
         </v-card>
-      </v-flex>  
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
--- src/components/Settings/Analytics/SoundDetection.vue
+++ src/components/Settings/Analytics/SoundDetection.vue
@@ -1,15 +1,15 @@
 <template>
-  <v-container fluid grid-list-xl>
+  <v-container fluid>
     <v-layout row wrap>
       <v-flex d-flex xs12>
-        <h1>{{ $t("sound_detection.title") }}</h1>
+        <span class="headline font-weight-bold">{{ $t("sound_detection.title") }}</span>
       </v-flex>
       <v-flex xs12 sm7 order-xs2 order-sm1>
-        <v-card>
-          <v-card-title class="yellow darken-1 mb-2">
-            <h3 class="headline">{{ $t("sound_detection.sound_detection_card_title") }}</h3>
+        <v-card class="my-3">
+          <v-card-title class="yellow darken-1 mb-3">
+            <span class="title">{{ $t("sound_detection.sound_detection_card_title") }}</span>
           </v-card-title>
-          <v-layout row wrap align-baseline>
+          <v-layout row wrap align-center>
              <v-flex d-flex xs12>
               <v-select class="px-3" :label="$t('sound_detection.sound_detection_mode')" :items="sound_detection_mode" v-model="current_sound_detection_mode"/>
             </v-flex>
@@ -19,29 +19,29 @@
                 <v-radio :disabled="!sound_detection_enabled" value="1" :label="$t('sound_detection.sensitivity_level')"/>
               </v-flex>
               <v-flex xs5 offset-xs1>
-                <v-select :disabled="!sound_detection_enabled" :items="sensitivity_levels" v-model="current_sensitivity_level"/> 
+                <v-select :disabled="!sound_detection_enabled" :items="sensitivity_levels" v-model="current_sensitivity_level"/>
               </v-flex>
               <v-flex xs6>
                 <v-radio :disabled="!sound_detection_enabled" value="2" :label="$t('sound_detection.customized_sensitivity_level')"/>
               </v-flex>
               <v-flex xs5 offset-xs1>
-                <v-slider :disabled="!sound_detection_enabled" v-model="customized_sensitivity_level" thumb-label="always" :thumb-size="24"/> 
+                <v-slider :disabled="!sound_detection_enabled" v-model="customized_sensitivity_level" thumb-label="always" :thumb-size="24"/>
               </v-flex>
-              </v-layout>    
+              </v-layout>
             </v-radio-group>
           </v-layout>
-        </v-card>    
+        </v-card>
       </v-flex>
       <v-flex xs12 sm5 order-xs1 order-sm2>
-        <v-card>
-          <v-card-title class="yellow darken-1 mb-2">
-            <h3 class="headline">{{ $t("player.title") }}</h3>
+        <v-card :class="{'mt-3': true, 'ml-0': $vuetify.breakpoint.xsOnly, 'ml-3': $vuetify.breakpoint.smAndUp}">
+          <v-card-title class="yellow darken-1 mb-3">
+            <span class="title">{{ $t("player.title") }}</span>
           </v-card-title>
           <v-card-text>
             <VideoPlayer/>
           </v-card-text>
         </v-card>
-      </v-flex>  
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
